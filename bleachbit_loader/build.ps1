0fa76d76c3f2cef9329fb1d9f0295c2a0d71a3bf
sodacris <wjxa20152015@gmail.com>
2025-02-09 23:43:19 +0800

    fix auto-build

--- bleachbit_loader/build.ps1
+++ bleachbit_loader/build.ps1
@@ -1,3 +1,6 @@
-Copy-Item -Recurse $PSScriptRoot/../bleachbit $PSScriptRoot
-Copy-Item -Recurse $PSScriptRoot/../data $PSScriptRoot
-Copy-Item -Recurse $PSScriptRoot/../cleaners $PSScriptRoot
+Copy-Item -Recurse -Force $PSScriptRoot/../bleachbit $PSScriptRoot
+Copy-Item -Recurse -Force $PSScriptRoot/../data $PSScriptRoot
+Copy-Item -Recurse -Force $PSScriptRoot/../cleaners $PSScriptRoot
+Copy-Item -Recurse -Force $PSScriptRoot/../windows $PSScriptRoot
+
+"111" > $PSScriptRoot/tmp.txt
